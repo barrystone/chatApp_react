f0ed9b23226484f1a4d6b4b941e5c636e6efe320
barrystone <barrystone3183@gmail.com>
2021-04-21 16:17:57 +0800

    [Update] sticky appBar and add custom server

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,40 +1,40 @@
-import React, { useState } from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-import AppBar from "./AppBar";
-import Messages from "./Messages";
-import PopupWindow from "./PopupWindow";
+import AppBar from './AppBar';
+import Messages from './Messages';
+import PopupWindow from './PopupWindow';
 
 //Conecting to apallo server
 const client = new ApolloClient({
-  // uri: "http://localhost:4000/",
-  uri: process.env.SERVER_URL,
-  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000/',
+  // uri: process.env.SERVER_URL,
+  cache: new InMemoryCache()
 });
 
 const Chat = ({ user, outChat }) => {
   const localTimelineColor = localStorage
-    .getItem("chatApp_react__chat-timelineColor")
-    .split(",");
+    .getItem('chatApp_react__chat-timelineColor')
+    .split(',');
 
   const [timelineColor, setTimelineColor] = useState({
     todayColor:
-      localTimelineColor[0] == null ? "#05c46b" : localTimelineColor[0],
+      localTimelineColor[0] == null ? '#05c46b' : localTimelineColor[0],
     last7DayColor:
-      localTimelineColor[1] == null ? "#ffd32a" : localTimelineColor[1],
+      localTimelineColor[1] == null ? '#ffd32a' : localTimelineColor[1],
     thisMonthColor:
-      localTimelineColor[2] == null ? "#000000" : localTimelineColor[2],
+      localTimelineColor[2] == null ? '#000000' : localTimelineColor[2]
   });
 
   const changeColor = (e) => {
     setTimelineColor({
       todayColor: e.nTodayColor,
       last7DayColor: e.nLast7DayColor,
-      thisMonthColor: e.nThisMonthColor,
+      thisMonthColor: e.nThisMonthColor
     });
 
     localStorage.setItem(
-      "chatApp_react__chat-timelineColor",
+      'chatApp_react__chat-timelineColor',
       `${e.nTodayColor},${e.nLast7DayColor},${e.nThisMonthColor}`
     );
   };
